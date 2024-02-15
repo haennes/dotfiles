860213b1230e347eb72a4b626e216ce7292979a7
hannses <example.com>
2024-02-15 22:06:19 +0100

    make wg ready to be a flake

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -3,36 +3,8 @@ with ips;
 let 
   hostname = config.networking.hostName;
   cfg = config.services.wireguard-wrapper;
-  nodes = { 
-    welt = {
-      ips = [
-        (ip_cidr welt.wg0)
-        (subnet_cidr lib welt.wg0)
-      ]; #first: own ip, all: ips that this peer can forward
-      endpoint = "hannses.de:51821"; # else null
-      }; 
-    porta.ips = [(ip_cidr porta.wg0)]; 
-    syncschlawiner.ips = [(ip_cidr syncschlawiner.wg0)];
-    syncschlawiner_mkhh.ips = [(ip_cidr syncschlawiner_mkhh.wg0)];
-    tabula.ips = [(ip_cidr tabula.wg0)];
-    thinkpad.ips = [(ip_cidr thinkpad.wg0)];
-    handy_hannses.ips = [(ip_cidr handy_hannses.wg0)];
-    mainpc.ips = [(ip_cidr mainpc.wg0)];
-  };
-  connections = [
-    # connect to specific interface like this: ["porat%wg0" "welt%wg1"]
-    ["tabula" "welt"]
-    ["porta" "welt"]
-    ["syncschlawiner" "welt"]
-    ["syncschlawiner_mkhh" "welt"]
-    ["handy_hannses" "welt"]
-    ["thinkpad" "welt"]
-    ["mainpc" "welt"]
-  ];
   secrets = import ../lib/wireguard;
-  priv_key = secrets.age_obtain_wireguard_priv{inherit hostname;};
-  our_connections = builtins.filter ( l: (builtins.elem hostname l)) connections;
-
+  our_connections = builtins.filter ( l: (builtins.elem hostname l)) cfg.connections;
 in
 with lib; with lib.types;{
   options.services.wireguard-wrapper = {
@@ -43,33 +15,72 @@ with lib; with lib.types;{
     };
     privateKeyFile = mkOption{
       type = path;
-      default = config.age.secrets."wireguard_${hostname}_wg0_private".path;
+    };
+    publicKey = mkOption{
+      type = anything;
+    };
+
+    port = mkOption {
+      type = types.port;
+      default = 51820;
+    };
+    openPort = mkOption {
+      type = bool;
+      default = true;
+    };
+    connections = mkOption { type = listOf (listOf str); };
+    nodes = mkOption {
+      type = attrsOf (submodule (
+      {
+        options = {
+          #devname = mkOption {
+          #  type = str;
+          #  default = name;
+          #  description = lib.mdDoc ''
+          #    The name of the device.
+          #  '';
+          #};
+          endpoint = mkOption {
+            type = nullOr str;
+            default = null;
+            description = mdDoc ''
+              set if this is an endpoint
+            '';
+            example = "wg.example.com:51820";
+          };
+          ips = mkOption {
+            type = listOf str;
+            description =
+              mdDoc "  first: own ip, all: ips that this peer can forward\n";
+          };
+        };
+      }));
     };
   };
   config = mkIf cfg.enable ({
-    networking.firewall.allowedUDPPorts = [51821];
+    networking.firewall.allowedUDPPorts = mkIf cfg.openPort [ cfg.port ];
 
     networking.wireguard.interfaces = {
       "wg0" = {
-        ips = [(builtins.head nodes."${hostname}".ips)];
+        ips = [(builtins.head cfg.nodes."${hostname}".ips)];
 	mtu = cfg.mtu;
 	privateKeyFile = cfg.privateKeyFile;
-	listenPort = 51821;
+	listenPort = cfg.port;
 	peers = (builtins.map (l:
           let 
 	    other = (lib.head (lib.remove hostname l));
-	    other_node = nodes."${other}";
+	    other_node = cfg.nodes."${other}";
           in 
 	  ({	 
 	    name = other; 
-	    publicKey = ((secrets.obtain_wireguard_pub{hostname = other;}).key); 
+	    publicKey = ((cfg.publicKey) other);
 	    allowedIPs = (other_node.ips);
 	    persistentKeepalive = 25;
-	    endpoint = (if other_node ? endpoint then other_node.endpoint else null);
+	    endpoint = other_node.endpoint;
 	  })
 	) our_connections);
       };
     };
 
-  } // priv_key);
+  });
 }
