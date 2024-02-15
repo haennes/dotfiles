1b88d350dd689302e19fe94140375f19af0802cf
hannses <example.com>
2024-02-15 22:06:19 +0100

    make wg ready to be a flake

--- lib/wireguard/default.nix
+++ lib/wireguard/default.nix
@@ -1,18 +1,16 @@
-#{lib, ...}:{
 {
-  age_obtain_wireguard_priv = {hostname, interface ? "wg0", base_folder ? "secrets/wireguard", ...}:{
+  age_obtain_wireguard_priv = {hostname, interface ? "wg0", ...}:{
     
     age.secrets.${"wireguard_${hostname}_${interface}_private"}  =  {
-        file = ../../${ "${base_folder}/${hostname}/${interface}/priv.age" };
+        file = ../../${ "secrets/wireguard/${hostname}/${interface}/priv.age" };
         owner = "root";
         group = "root";
       };
     };
   obtain_wireguard_pub = {hostname, interface ? "wg0", base_folder ? "secrets/wireguard", ...}:{
     key = (import ../../${ "${base_folder}/${hostname}/${interface}/pub.nix"}).key;
-  };
+    };
   
-
   age_generate_wireguard_keypair = {hostname, publicKeys, interface ? "wg0", base_folder ? "wireguard", ...}:{
     "${base_folder}/${hostname}/${interface}/priv.age".publicKeys = publicKeys;
     #"${base_folder}/${hostname}/${interface}/pub.age".publicKeys = publicKeys;
--- modules/all/base.nix
+++ modules/all/base.nix
@@ -8,12 +8,18 @@ let
     grub.device = "/dev/sda15";
     grub.forceInstall = true;
   };
+  secrets = import ../../lib/wireguard;
+  priv_key = hostname: secrets.age_obtain_wireguard_priv{inherit hostname;};
 in
-{config, pkgs, lib, vps ? false, proxmox ? false, ...}:
+{config, pkgs, lib, ips, vps ? false, proxmox ? false, ...}:
 with lib;
+with ips;
+let
+  simple_ip = name: { "${name}".ips = [ (ip_cidr ips."${name}".wg0)]; };
+in
 {
-  config.boot.loader = mkIf (!proxmox) (if vps then ovh_custom_bootl else default_bootl);
   config = {
+  boot.loader = mkIf (!proxmox) (if vps then ovh_custom_bootl else default_bootl);
   system.stateVersion = "23.11"; # Did you read the comment?
   
   # Make ready for nix flakes
@@ -55,5 +61,39 @@ with lib;
   nixpkgs.config.allowUnfree = true;
   # Enable the OpenSSH daemon.
   services.openssh.enable = true; #TODO: extract into module 
+
+
+  services.wireguard-wrapper = {
+    #port = 51821;
+    connections = [
+      ["tabula" "welt"]
+      ["porta" "welt"]
+      ["syncschlawiner" "welt"]
+      ["syncschlawiner_mkhh" "welt"]
+      ["handy_hannses" "welt"]
+      ["thinkpad" "welt"]
+      ["mainpc" "welt"]
+    ];
+    nodes = { 
+      welt = {
+        ips = [
+          (ip_cidr welt.wg0)
+          (subnet_cidr lib welt.wg0)
+        ]; 
+        endpoint = "hannses.de:51821"; 
+      }; 
+    }
+    // simple_ip "porta"
+    // simple_ip "syncschlawiner"
+    // simple_ip "syncschlawiner_mkhh"
+    // simple_ip "tabula"
+    // simple_ip "thinkpad"
+    // simple_ip "mainpc"
+    // simple_ip "handy_hannses"
+    ;
+    publicKey = name: ((secrets.obtain_wireguard_pub{hostname = name;}).key);
+    privateKeyFile = config.age.secrets."wireguard_${config.networking.hostName}_wg0_private".path;
+    port = 51821;
   };
+} // priv_key config.networking.hostName;
 }
