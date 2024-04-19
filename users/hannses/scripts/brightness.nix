91ab757bb44bea35ca4284c782842bb2e4c637ed
haennes <hannes.hofmuth@gmail.com>
2024-04-19 18:46:17 +0200

    simplified brightness + cross-platfrom

--- users/hannses/scripts/brightness.nix
+++ users/hannses/scripts/brightness.nix
@@ -5,21 +5,18 @@
 
     DMENU="${globals.dmenu}"
 
-    levels="0
+    levels="+5
+    5-
     5
     20
     60
     99"
 
-    step=$(($(cat /sys/class/backlight/intel_backlight/max_brightness) / 100))
-    current=$(($(cat /sys/class/backlight/intel_backlight/brightness) / $step))
-
     end () {
         if [ -n "$value" ]; then
-            if [ "$value" -le "100" ]; then
-                echo $(($value * $step)) > /sys/class/backlight/intel_backlight/brightness
-            fi
-            notify-send -a "brightness" "set brightness to $value"
+            ${pkgs.brightnessctl}/bin/brightnessctl  s $value%
+            current=$(${pkgs.brightnessctl}/bin/brightnessctl -m  | grep -o '[^,]\+' | sed -n '4p')
+            notify-send -a "brightness" "set brightness to $current"
         fi
         exit 0
     }
@@ -27,25 +24,11 @@
     getopts "ids:" flag
     case "''${flag}" in
         i) 
-            if [ "$current" -eq "0" ]; then
-                value=1
-            elif [ "$current" -eq "1" ]; then
-                value=5
-            elif [ "$(($current + 5))" -gt "100" ]; then
-                value=100
-            else
-                value=$(($current + 5))
-            fi
+            value = "+5"
             end
             ;;
         d)
-            if [ "$current" -eq "1" ]; then
-                value=0
-            elif [ "$(($current - 5))" -lt "1" ]; then
-                value=1
-            else
-                value=$(($current - 5))
-            fi
+            value = "5-"
             end
             ;;
         s)
