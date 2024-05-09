afa23b605809929270ddf969fa81455a7b97febf
haennes <hannes.hofmuth@gmail.com>
2024-05-09 13:19:59 +0200

    wpaperd

--- users/hannses/scripts/startup.nix
+++ users/hannses/scripts/startup.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }: {
   startup = pkgs.pkgs.writeShellScript "startup" ''
     ${pkgs.waybar}/bin/waybar &
-    ${pkgs.swww}/bin/swww init
-    $(cat ~/.wallpapercall)
-
-    # start wallpaper using my wallpaper script
+    ${pkgs.wpaperd}/bin/wpaperd
   '';
 }
