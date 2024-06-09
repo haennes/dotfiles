ba20c76990d627e2190bbd1f25edd25d4fd4de8a
haennes <hannes.hofmuth@gmail.com>
2024-06-09 12:18:12 +0200

    more gh aliases

--- users/hannses/firefox.nix
+++ users/hannses/firefox.nix
@@ -134,7 +134,7 @@ in {
           }];
           icon =
             "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "<npkgs" ];
+          definedAliases = [ "<npkgs" "<ghnpgks" "<ghnp" ];
         };
         "NixOS Wiki" = {
           urls = [{
