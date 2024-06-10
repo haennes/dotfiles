6e862679b19779e5f4339fdfb3dd76c0b1c05d85
haennes <hannes.hofmuth@gmail.com>
2024-06-10 17:00:41 +0200

    fdroid

--- users/hannses/firefox.nix
+++ users/hannses/firefox.nix
@@ -234,6 +234,14 @@ in {
           iconUpdateURL = favicon "simpleicons.org";
           inherit updateInterval;
         };
+        "fdroid" = {
+          urls = [{
+            template = "https://search.f-droid.org/?q={searchTerms}";
+          }];
+          iconUpdateURL = "https://f-droid.org/assets/fdroid-logo_bfHl7nsLHOUQxzdU8-rGIhn4bAgl6z7k2mA3fWoCyT4=.png";
+          definedAliases = ["<fdroid" ];
+          inherit updateInterval;
+        };
 
         "Startpage" = {
           urls = [{
