7eca2031a97258b3fe2ff8fa2c70a664ba020918
haennes <hannes.hofmuth@gmail.com>
2024-06-17 21:33:09 +0200

    fix and update nc

--- servers/syncschlawiner/nextcloud.nix
+++ servers/syncschlawiner/nextcloud.nix
@@ -11,7 +11,7 @@ with lib; {
   services.nextcloud = {
     enable = true;
     hostName = "localhost";
-    package = pkgs.nextcloud28;
+    package = pkgs.nextcloud29;
     https = true;
 
     home = "/data/nextcloud";
@@ -35,7 +35,7 @@ with lib; {
       ];
     };
 
-    maxUploadSize = "2G";
+    maxUploadSize = "4G";
     #notify_push.enable = true;
     #configureRedis = true;
     #caching.redis = true;
@@ -44,29 +44,7 @@ with lib; {
     extraApps = with config.services.nextcloud.package.packages.apps; {
 
       inherit contacts calendar deck groupfolders maps bookmarks cookbook
-        cospend forms mail notes onlyoffice polls tasks;
-      memories = pkgs.fetchNextcloudApp {
-        sha256 = "sha256-Xr1SRSmXo2r8yOGuoMyoXhD0oPVm/0/ISHlmNZpJYsg=";
-        url =
-          "https://github.com/pulsejet/memories/releases/download/v6.2.2/memories.tar.gz";
-        license = "lgpl3"; # should be agpl but not in set anymore ??
-      };
-      familytree = pkgs.fetchNextcloudApp {
-        sha256 = "sha256-Lj20mNni8XxWSij1p7IvjC6PdhkEtWcglc1BwZuPHlc=";
-        url =
-          "https://github.com/derbenx/nextcloud-familytree/releases/download/1.3.5/familytree.tar.gz";
-        license = "lgpl3"; # see abouve
-      };
-      #news = pkgs.fetchNextcloudApp {
-      #  sha256 = "sha256-cfJkKRNSz15L4E3w1tnEb+t4MrVwVzb8lb6vCOA4cK4=";
-      #  url = "https://github.com/nextcloud/news/releases/download/24.0.0/news.tar.gz";
-      #  license = "agpl3";
-      #};
-      #integration_google = pkgs.fetchNextcloudApp {
-      #sha256 = "sha256-ZOeuJ8DQr36mUt9svHmc+wJ46qof7kxKyZP9WcEtgTs=";
-      #url = "https://github.com/nextcloud/integration_google/releases/download/v1.0.9/integration_google-1.0.9.tar.gz";
-      #license = "agpl3";
-      #};
+        cospend forms mail notes polls;
     };
     # extraAppsEnable = true;
     #notify_push.enable = true;
