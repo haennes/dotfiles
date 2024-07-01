fdada929d13422c180cda4666a56ee5ffa56e968
haennes <hannes.hofmuth@gmail.com>
2024-07-01 20:25:40 +0200

    acme restructure

--- modules/all/default.nix
+++ modules/all/default.nix
@@ -1,5 +1,5 @@
 {lib,  ... }: {
-  imports = [ ./base.nix ./pkgs.nix ./store_optimize.nix ./nm-fix.nix ];
+  imports = [ ./base.nix ./pkgs.nix ./store_optimize.nix ./nm-fix.nix ./acme.nix];
 
   options = {
     nextcloud_max_size = lib.mkOption{
