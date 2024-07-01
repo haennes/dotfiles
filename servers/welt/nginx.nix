b4e9fd318dbf012202328f7f37f0595b82d10809
haennes <hannes.hofmuth@gmail.com>
2024-07-01 20:25:40 +0200

    acme restructure

--- servers/welt/nginx.nix
+++ servers/welt/nginx.nix
@@ -1,13 +1,18 @@
 { lib, config, ips, ... }:
 let
   create_simple_proxy_with_domain =
-    { fqdn, target_ip, custom_settings ? { }, custom_locations ? { } }: {
+    { fqdn, target_ip, custom_settings ? { }, custom_locations ? { }, target_port ? null, https ? false }:
+    let
+    target_port_str = (if target_port == null then "" else ":${builtins.toString target_port}");
+    https_str = (if https then "s" else "");
+    in
+    {
       security.acme.certs."${fqdn}" = { inheritDefaults = true; };
       services.nginx.virtualHosts."${fqdn}" = {
         enableACME = true;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://${target_ip}";
+          proxyPass = "http${https_str}://${target_ip}${target_port_str}";
           proxyWebsockets = true; # needed if you need to use WebSocket
           #extraConfig =
           ## required when the target is also TLS server with multiple hosts
@@ -20,14 +25,7 @@ let
   recursiveMerge = listOfAttrsets:
     lib.fold (attrset: acc: lib.recursiveUpdate attrset acc) { } listOfAttrsets;
 in {
-
 } // recursiveMerge [
-  {
-    security.acme = {
-      acceptTerms = true;
-      defaults.email = "vegsy5q8@anonaddy.me";
-    };
-  }
   {
     services.nginx = {
       enable = true;
@@ -42,9 +40,16 @@ in {
   (create_simple_proxy_with_domain {
     fqdn = "cloud.hannses.de";
     target_ip = ips.syncschlawiner.wg0;
+    https = true;
     custom_settings = {
     extraConfig = "client_max_body_size ${config.nextcloud_max_size };";
     };
+  })
+  (create_simple_proxy_with_domain {
+    fqdn = "kasmweb.hannses.de";
+    https = true;
+    target_ip = ips.syncschlawiner.wg0;
+    target_port = 8001;
   })
   #(create_simple_proxy_with_domain{fqdn = "mail.hannses.de"; target_ip = ips.tabula.wg0;})
   (create_simple_proxy_with_domain {
