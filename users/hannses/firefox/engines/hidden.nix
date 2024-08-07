8723f8c2e1232962d233d5d7f19960e693e6174f
haennes <hannes.hofmuth@gmail.com>
2024-08-07 22:00:36 +0200

    hide default search engines

---
+++ users/hannses/firefox/engines/hidden.nix
@@ -0,0 +1,7 @@
+{...}:{
+  "Wikipedia (en)".metaData.hidden = true;
+  "Amazon.de".metaData.hidden = true;
+  "Bing".metaData.hidden = true;
+  "DuckDuckGo".metaData.hidden = true;
+  "Google".metaData.hidden = true;
+}
