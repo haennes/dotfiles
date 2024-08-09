8aacb4f5104760b865d0af9d0e513534228099a9
haennes <hannes.hofmuth@gmail.com>
2024-08-09 16:51:45 +0200

    homemanager cleanup

--- modules/home_manager/default.nix
+++ modules/home_manager/default.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, pkgs,  config, nur, ips, sshkeys, overlays, permit_pkgs, ports, ... }@all_inputs:
+{ inputs, pkgs,  config, nur, ips, sshkeys, overlays, permit_pkgs, ports, ... }@all_inputs:
 let build_user = name: { ${name} = import ../../users/${name}; };
 in {
   home-manager = {
@@ -8,14 +8,10 @@ in {
       addons = nur.repos.rycee.firefox-addons;
       outer_config = config;
       inherit (inputs) nixvim;
-      inherit inputs;
-      inherit ips;
-      inherit ports;
-      inherit sshkeys;
-      inherit overlays;
-      inherit permit_pkgs;
+      inherit inputs ips ports sshkeys overlays permit_pkgs;
+      #TODO this is dumb, import them at a user level or make them user bound and move them here
       theme = import ../../users/hannses/theme.nix;
-      globals = import ../../users/hannses/globals.nix { inherit pkgs; };
+      globals = import ../../users/hannses/globals.nix all_inputs;
       scripts = import ../../users/hannses/scripts all_inputs;
       gnome_enable = config.services.xserver.desktopManager.gnome.enable;
       virt-manager_enable = config.programs.virt-manager.enable;
