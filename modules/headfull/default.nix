cc9c6f3a89fa0261f241df0dff4d6e1e1f072570
haennes <hannes.hofmuth@gmail.com>
2024-08-09 17:27:58 +0200

    extract virtualization into own modules

--- modules/headfull/default.nix
+++ modules/headfull/default.nix
@@ -1,4 +1,4 @@
 { config, lib, ... }: {
-  imports = [ ./base.nix ./pkgs.nix ./tasks.nix ./homepage-dashboard.nix ./local_nginx.nix ./atuin.nix ./fortivpn.nix];
+  imports = [ ./base.nix ./pkgs.nix ./tasks.nix ./homepage-dashboard.nix ./local_nginx.nix ./atuin.nix ./fortivpn.nix ./virtualization.nix];
 
 }
