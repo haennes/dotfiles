9df94992f8c49615035364cab1d7f676ea487bb1
haennes <hannes.hofmuth@gmail.com>
2024-08-11 11:20:45 +0200

    minecwaft

---
+++ modules/headfull/minecraft/default.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}:{
+
+  environment = {
+    systemPackages = with pkgs; [
+      atlauncher                          #minecraft launcher with mod support
+      portablemc                          #minecraft launcher cmd
+    ];
+
+    etc = {
+      "minecraft_deps/jre8".source = pkgs.jre8.outPath;
+    };
+  };
+}
