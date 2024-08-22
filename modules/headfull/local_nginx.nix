48914f7cf3f127694ebaff5c10ccba2db10e66ef
haennes <hannes.hofmuth@gmail.com>
2024-08-22 23:13:16 +0200

    clean up local nginx

--- modules/headfull/local_nginx.nix
+++ modules/headfull/local_nginx.nix
@@ -1,13 +1,9 @@
 { lib, config, ... }:
 let
-linking = {
-      #TODO make this auto-updating
-      "/syncschlawiner_syncthing" = 8385;
-      "/syncschlawiner_web_80" = 8386;
-      "/synscchlawiner_web_443" = 8387;
-      #"/home" = config.services.homepage-dashboard.listenPort;
-      "/" = config.services.homepage-dashboard.listenPort;
-    };
+  linking = {
+    sync = 8384;
+    "s.sync" = 8385;
+  };
 in
 {
   # the tasks ones are managed in tasks.nix
@@ -18,27 +14,11 @@ in
     recommendedTlsSettings = true;
   };
   services.nginx.virtualHosts= {
-    "localhost".locations = {
-      "/" = {
-       proxyPass = "http://localhost:${toString config.services.homepage-dashboard.listenPort}";
-      };
-      "/syncthing" = {return = "308 http://localhost:8384";};
-    };
-    "sync.localhost".locations."/" = {
-      proxyPass = "http://localhost:8384";
-    };
-    "s.sync.localhost".locations."/" = {
-      proxyPass = "http://localhost:8385";
-    };
-  };
-  #services.nginx.virtualHosts."localhost".locations = lib.mapAttrs
-  #  (name: value: {
-  #    proxyPass = "http://localhost:${toString value}";
-  #    #return = "308 http://localhost:${toString value}";
-  #    #recommendedProxySettings = false;
-  #    proxyWebsockets = true;
-  #    } // (if name == "/" then {} else {priority = 100;})) linking;
-  #services.nginx.virtualHosts."127.0.0.1".locations = lib.mapAttrs
-  #  (name: value: { proxyPass = "https://127.0.0.1:${toString value}"; }) linking;
-
+    "localhost".locations."/".proxyPass = "http://localhost:${toString config.services.homepage-dashboard.listenPort}";
+  } // ( lib.mapAttrs'
+      (name: value: {
+        name = "${name}.localhost";
+        value = { locations."/".proxyPass = "http://localhost:${toString value}";};
+      })
+  linking );
 }
