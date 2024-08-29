5c37c2879ebb1198396035cd8d10a7eabf754f14
haennes <hannes.hofmuth@gmail.com>
2024-08-29 18:29:20 +0200

    fix import moved to wrong line

--- modules/headfull/default.nix
+++ modules/headfull/default.nix
@@ -1,5 +1,5 @@
-  imports = [ ./base.nix ./pkgs.nix ./minecraft ./tasks.nix ./homepage-dashboard.nix ./local_nginx.nix ./atuin.nix ./fortivpn.nix ./virtualization.nix ];
 { config, lib, hports, ... }: {
+  imports = [ ./base.nix ./pkgs.nix ./minecraft ./tasks.nix ./homepage-dashboard.nix ./local_nginx.nix ./atuin.nix ./fortivpn.nix ./virtualization.nix ];
   services.postgresql.settings.port = lib.mkIf (hports ? postresql) hports.postgresql;
 
 }
