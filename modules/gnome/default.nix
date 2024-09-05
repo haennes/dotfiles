7268703dcd6e055655a85167f668135c8deecca7
haennes <hannes.hofmuth@gmail.com>
2024-09-05 20:53:38 +0200

    gnome pkgs moved

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -9,25 +9,22 @@ with lib; {
       };
       displayManager.gdm.enable = true;
     };
-    environment.gnome.excludePackages =
-      (with pkgs; [
-        gnome-photos
-        gnome-tour
-        gnome-console
-        cheese # webcam tool
-        epiphany # web browser
-        geary # email reader
-        yelp # Help view
-      ])
-      ++ (with pkgs.gnome; [
-        gnome-music
-        tali # poker game
-        iagno # go game
-        hitori # sudoku game
-        atomix # puzzle game
-        gnome-contacts
-        gnome-initial-setup
-      ]);
+    environment.gnome.excludePackages = with pkgs; [
+      gnome-photos
+      gnome-tour
+      gnome-console
+      cheese # webcam tool
+      epiphany # web browser
+      geary # email reader
+      yelp # Help view
+      gnome-music
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+      gnome-contacts
+      gnome-initial-setup
+    ];
     programs.dconf.enable = true;
     environment.systemPackages = with pkgs; [ gnome-tweaks ];
   };
