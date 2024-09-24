42e360bdfb3fb69d80faef21b2fb19314fcd1c18
haennes <hannes.hofmuth@gmail.com>
2024-09-24 19:32:01 +0200

    extract helix

--- users/hannses/default.nix
+++ users/hannses/default.nix
@@ -18,6 +18,7 @@
     #update broke ./vimiv.nix # images
     ./codium.nix
     ./vim.nix
+    ./helix.nix
     ./yazi
     ./firefox
     ./ssh.nix
@@ -105,42 +106,6 @@
   ];
 
   programs.zathura.enable = true;
-  programs.helix = {
-    enable = true;
-    #defaultEditor = true; # leave nvim for now
-    languages = {
-      language-server = {
-        rust-analyzer = {
-          config.check.command = "clippy";
-          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
-        };
-        nil = { command = "${pkgs.nil}/bin/nil"; };
-        ruff-lsp.command = "${pkgs.ruff}/bin/ruff";
-      };
-      language = [
-        {
-          name = "rust";
-          auto-format = true;
-        }
-        {
-          name = "nix";
-          formatter = { command = "nixpkgs-fmt"; };
-          auto-format = true;
-        }
-        {
-          name = "python";
-          language-servers = [ "ruff-lsp" ];
-
-          # In case you'd like to use ruff alongside black for code formatting:
-          formatter = {
-            command = "black";
-            args = [ "--quiet" "-" ];
-          };
-          auto-format = true;
-        }
-      ];
-    };
-  };
   # Environment
   home.sessionVariables = {
     EDITOR = "nvim";
---
+++ users/hannses/helix.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }: {
+  programs.helix = {
+    enable = true;
+    #defaultEditor = true; # leave nvim for now
+    languages = {
+      language-server = {
+        rust-analyzer = {
+          config.check.command = "clippy";
+          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+        };
+        nil = { command = "${pkgs.nil}/bin/nil"; };
+        ruff-lsp.command = "${pkgs.ruff}/bin/ruff";
+      };
+      language = [
+        {
+          name = "rust";
+          auto-format = true;
+        }
+        {
+          name = "nix";
+          formatter = { command = "nixpkgs-fmt"; };
+          auto-format = true;
+        }
+        {
+          name = "python";
+          language-servers = [ "ruff-lsp" ];
+
+          # In case you'd like to use ruff alongside black for code formatting:
+          formatter = {
+            command = "black";
+            args = [ "--quiet" "-" ];
+          };
+          auto-format = true;
+        }
+      ];
+    };
+  };
+}
