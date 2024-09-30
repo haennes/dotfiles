c1e0d352c74e901f49f02ccaa6506622011b28b1
haennes <hannes.hofmuth@gmail.com>
2024-09-30 23:28:05 +0200

    agenix r

--- servers/tabula/default.nix
+++ servers/tabula/default.nix
@@ -1,6 +1,73 @@
-{ ... }: {
+{ inputs, config, lib, sshkeys, ... }:
+let
+  ips = config.ips.ips.ips.default;
+  hports = config.ports.ports.curr_ports;
+in {
 
-  imports = [ ../proxmox.nix ./nginx.nix ];
+  microvm = {
+    #...add additional MicroVM configuration here
+    interfaces = [{
+      type = "tap";
+      id = "vm-tabula";
+      mac = "${config.macs.macs.vm-host.vm-tabula.eth0}";
+    }];
+
+    shares = [{
+      source = "/home/hannses/ssh/";
+      mountPoint = "/persist";
+      tag = "persist";
+      proto = "virtiofs";
+    }];
+  };
+  system.activationScripts.ensure-ssh-key-dir.text = "mkdir -p /persist";
+  services.openssh = {
+    hostKeys = [
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
+  };
+  fileSystems."/persist".neededForBoot = lib.mkForce true;
+  age.identityPaths = [ "/persist/root_user_key" ];
+  imports = [
+    #../../secrets/macs.nix
+    #../../secrets/ips.nix
+    #../../secrets/ports.nix
+    #../../modules/all
+    #../../modules/headless
+    #inputs.IPorts.nixosModules.default
+    #inputs.wireguard-wrapper.nixosModules.default
+    #inputs.syncthing-wrapper.nixosModules.default
+    #inputs.agenix.nixosModules.default
+    #../proxmox.nix
+    ./nginx.nix
+  ];
+
+  systemd.network.networks."20-lan" = {
+    matchConfig.Type = "ether";
+    networkConfig = {
+      Address = [ "${ips.vm-tabula.br0}/24" "2001:db8::b/64" ];
+      Gateway = ips."vm-host".br0;
+      DNS = [ ips."vm-host".br0 ];
+      #DNS = [ "1.1.1.1" ];
+      IPv6AcceptRA = true;
+      DHCP = "no";
+    };
+  };
+
+  networking.useDHCP = lib.mkForce false;
+
+  services.openssh = {
+    enable = true; # TODO: extract into module
+    ports = [ hports.sshd ];
+  };
+  users.users.root.openssh.authorizedKeys.keys = [ sshkeys.hannses ];
 
   services.syncthing_wrapper = { enable = true; };
   services.syncthing = {
