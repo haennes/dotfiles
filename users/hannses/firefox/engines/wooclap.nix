575a77434c169a9215b9af1da19cc807c8a8afc4
haennes <hannes.hofmuth@gmail.com>
2024-10-10 12:14:48 +0200

    wooclap and cpp search

---
+++ users/hannses/firefox/engines/wooclap.nix
@@ -0,0 +1,10 @@
+{ updateInterval, ... }: {
+  # Dont ask why (:
+  "wooclap" = {
+    urls = [{ template = "https://app.wooclap.com/{searchTerms}"; }];
+    iconUpdateURL =
+      "https://wooclap-assets-production.wooclap.com/11.83.0-47c1c0efee233d790dc49acfc76f32f4f91ceba0/assets/favicon.ico";
+    definedAliases = [ "<woo" "<clap" ];
+    inherit updateInterval;
+  };
+}
