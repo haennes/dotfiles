529af4513ec0d869aa6c17d78badc675878b1f11
haennes <hannes.hofmuth@gmail.com>
2024-10-16 10:37:40 +0200

    adb udevrules apply

--- modules/headfull/adb.nix
+++ modules/headfull/adb.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   programs.adb.enable = true;
   users.users.hannses.extraGroups = [ "adbusers" "kvm" ];
-  #services.udev.packages = with pkgs; [ android-udev-rules ];
+  services.udev.packages = with pkgs; [ android-udev-rules ];
 }
