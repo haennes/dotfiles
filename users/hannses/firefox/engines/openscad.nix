0d538c4737ed58dd870291058224d68466072527
haennes <hannes.hofmuth@gmail.com>
2024-10-21 20:55:32 +0200

    openscad search

---
+++ users/hannses/firefox/engines/openscad.nix
@@ -0,0 +1,12 @@
+{ updateInterval, ... }: {
+  "BOSL" = {
+    urls = [{
+      template =
+        "https://github.com/search?q=repo%3ABelfrySCAD%2FBOSL2+{searchTerms}&type=wikis";
+    }];
+    iconUpdateURL =
+      "https://github.com/BelfrySCAD/BOSL2/blob/master/images/BOSL2logo.png?raw=true";
+    definedAliases = [ "<bosl" ];
+    inherit updateInterval;
+  };
+}
