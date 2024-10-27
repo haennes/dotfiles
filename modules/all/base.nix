2062b1f63cd2506f48fcabff5d24183eac0343f2
haennes <hannes.hofmuth@gmail.com>
2024-10-27 17:39:22 +0100

    add hyprland cache

--- modules/all/base.nix
+++ modules/all/base.nix
@@ -9,7 +9,13 @@ with lib; {
 
     nix = {
       package = pkgs.nix;
-      settings.experimental-features = [ "nix-command" "flakes" ];
+      settings = {
+        substituters = [ "https://hyprland.cachix.org" ];
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        ];
+        experimental-features = [ "nix-command" "flakes" ];
+      };
     };
 
     time.timeZone = "Europe/Berlin";
