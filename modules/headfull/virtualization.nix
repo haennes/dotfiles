c30b70de6ed4cd097eedea7369eb26f9a1f73fca
haennes <hannes.hofmuth@gmail.com>
2024-10-30 23:33:50 +0100

    more virtualization

--- modules/headfull/virtualization.nix
+++ modules/headfull/virtualization.nix
@@ -1,10 +1,31 @@
-{ ... }: {
-  virtualisation.libvirtd.enable = true;
+{ pkgs, lib, ... }: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
 
   virtualisation.docker.enable = true;
 
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
+  # dont build virtualbox
+  #virtualisation.virtualbox.host.enable = true;
+  #virtualisation.virtualbox.host.enableExtensionPack = true;
 
-  programs.virt-manager.enable = true; # };
+  virtualisation.waydroid.enable = true;
+  systemd.services.waydroid-container.wantedBy = lib.mkForce [ ];
+
+  programs.virt-manager.enable = true;
+
 }
