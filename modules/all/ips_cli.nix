4a0649ee94ef661d3fef6b91b6456ce7dec89cfc
haennes <hannes.hofmuth@gmail.com>
2024-11-04 17:55:56 +0100

    search for ips and ports

---
+++ modules/all/ips_cli.nix
@@ -0,0 +1,26 @@
+{ lib, config, pkgs, ... }:
+let
+  inherit (lib)
+    mapAttrsToList concatStringsSep hasPrefix partition naturalSort reverseList
+    flatten;
+  _split_delim = "/";
+  ips = config.ips.ips.ips.default;
+  ips_list = flatten (mapAttrsToList (hostname: interfaces:
+    mapAttrsToList (if_name: if_ip:
+      "${hostname}${_split_delim}${if_name}${_split_delim}${if_ip}") interfaces)
+    ips);
+  ips_file = pkgs.writeText "ips_file" ((concatStringsSep "\n") ips_list);
+
+  ips_app = pkgs.writeShellApplication {
+    name = "ips";
+    checkPhase = ":"; # too many failed checks
+    bashOptions = [ ]; # unbound variable $1
+    text = ''
+      all=$(cat ${ips_file})
+      echo "$all" | column -s${_split_delim} -t --table-noheadings --table-columns HOSTNAME,INTERFACE,PORT --table-right PORT | ${pkgs.fzf}/bin/fzf
+    '';
+  };
+in {
+
+  environment.systemPackages = [ ips_app ];
+}
