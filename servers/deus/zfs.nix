a971c31413c48cf12797f9edd31b504c0a8b298c
haennes <hannes.hofmuth@gmail.com>
2024-11-04 22:16:06 +0100

    better zfs mounting

--- servers/deus/zfs.nix
+++ servers/deus/zfs.nix
@@ -1,26 +1,14 @@
-{ ... }: {
+{ lib, ... }: {
   networking.hostId = "d395beb7";
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
 
-  fileSystems = {
-    "/data" = {
-      device = "main_pool/data";
+  fileSystems = let inherit (lib) listToAttrs;
+  in listToAttrs (map (n: {
+    name = "/${n}";
+    value = {
+      device = "main_pool/${n}";
       fsType = "zfs";
     };
-
-    "/website" = {
-      device = "main_pool/website";
-      fsType = "zfs";
-    };
-
-    "/persistant" = {
-      device = "main_pool/persistance";
-      fsType = "zfs";
-    };
-    "/ankisync" = {
-      device = "main_pool/ankisync";
-      fsType = "zfs";
-    };
-  };
+  }) [ "data" "website" "persistant" "ankisync" ]);
 }
