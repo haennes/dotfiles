a6496ea776148de755e4d533a1dd9a82087b1dee
haennes <hannes.hofmuth@gmail.com>
2024-11-06 00:20:30 +0100

    turns out renaming a zfs pool is dumb

--- servers/deus/zfs.nix
+++ servers/deus/zfs.nix
@@ -3,12 +3,17 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
 
-  fileSystems = let inherit (lib) listToAttrs;
+  fileSystems = (let inherit (lib) listToAttrs;
   in listToAttrs (map (n: {
     name = "/${n}";
     value = {
       device = "main_pool/${n}";
       fsType = "zfs";
     };
-  }) [ "data" "website" "persistant" "ankisync" ]);
+  }) [ "data" "website" "ankisync" "kasmweb" "git" ]) // {
+    "persistance" = {
+      device = "main_pool/persistant";
+      fsType = "zfs";
+    };
+  });
 }
