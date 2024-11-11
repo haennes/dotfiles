2f0260ae67d3618eac6cc36c54d60d8e828c5861
haennes <hannes.hofmuth@gmail.com>
2024-11-12 00:31:58 +0100

    fix pgdev

--- modules/headfull/pg_dev.nix
+++ modules/headfull/pg_dev.nix
@@ -12,5 +12,14 @@
     initialScript = pkgs.writeText "postgresql-password" ''
       CREATE ROLE strichliste-rs WITH LOGIN PASSWORD 'secret' CREATEDB;
     '';
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser origin-address auth-method
+      # ipv4
+      local all       all     trust
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
+    '';
   };
+  #users.users.strichliste-rs.isNormalUser = true;
 }
