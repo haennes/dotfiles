b118187eab7922dbeabfad0f461ae2712bef2eb5
haennes <hannes.hofmuth@gmail.com>
2024-12-01 19:59:18 +0100

    completely disable distributed builds on deus

--- modules/all/distributed_builds.nix
+++ modules/all/distributed_builds.nix
@@ -1,16 +1,18 @@
-{ config, lib, ... }:
-lib.mkIf (!config.is_microvm) {
-  nix.buildMachines = lib.mkIf (config.services.wireguard-wrapper.enable
-    && config.networking.hostName != "deus") [{
-      hostName = config.ips.ips.ips.default.deus.wg0;
-      protocol = "ssh-ng";
-      sshUser = "forward";
-      sshKey = "/home/hannses/.ssh/id_ed25519_forward";
-      system = "x86_64-linux";
-      supportedFeatures = [ "kvm" "nixos-test" "big-parallel" "benchmark" ];
-      maxJobs = 16;
-    }];
-  nix.distributedBuilds = true;
-  # optional, useful when the builder has a faster internet connection than yours
-  nix.extraOptions = "  builders-use-substitutes = true\n";
+{ config, lib, ... }: {
+  config = lib.mkIf (!config.is_microvm
+    && config.services.wireguard-wrapper.enable && config.networking.hostName
+    != "deus") {
+      nix.buildMachines = [{
+        hostName = config.ips.ips.ips.default.deus.wg0;
+        protocol = "ssh-ng";
+        sshUser = "forward";
+        sshKey = "/home/hannses/.ssh/id_ed25519_forward";
+        system = "x86_64-linux";
+        supportedFeatures = [ "kvm" "nixos-test" "big-parallel" "benchmark" ];
+        maxJobs = 16;
+      }];
+      nix.distributedBuilds = true;
+      # optional, useful when the builder has a faster internet connection than yours
+      nix.extraOptions = "  builders-use-substitutes = true\n";
+    };
 }
