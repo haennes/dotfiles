3dbe166e33e76e712bcd6a1bbe80ccf2c1cc49cf
haennes <hannes.hofmuth@gmail.com>
2024-12-08 23:50:10 +0100

    thunderbird startup

--- users/hannses/scripts/startup.nix
+++ users/hannses/scripts/startup.nix
@@ -8,5 +8,6 @@ lib.filter (a: a != "") (lib.splitString "\n"
     [workspace 8 silent] ${pkgs.keepassxc}/bin/keepassxc
     [workspace 9 silent] ${pkgs.signal-desktop}/bin/signal-desktop
     [workspace 9 silent] ${pkgs.element-desktop}/bin/element-desktop
+    [workspace 10 silent] ${pkgs.thunderbird}/bin/thunderbird
   '')
 
