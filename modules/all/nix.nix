bb264e280b35b96156bb0f1a832c5ef5ee8662e6
haennes <hannes.hofmuth@gmail.com>
2024-12-11 23:20:34 +0100

    nix-community cache

--- modules/all/nix.nix
+++ modules/all/nix.nix
@@ -2,9 +2,11 @@
   nix = {
     package = pkgs.nix;
     settings = {
-      substituters = [ "https://hyprland.cachix.org" ];
+      substituters =
+        [ "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       # Make ready for nix flakes
       experimental-features = [ "nix-command" "flakes" ];
