34cb8ac5209b6e83cb428c946b823d195d4152bd
haennes <hannes.hofmuth@gmail.com>
2024-12-24 23:27:04 +0100

    better cross compile

--- modules/all/crosscompile.nix
+++ modules/all/crosscompile.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }: {
-  config = lib.mkIf
-    (!config.is_microvm && pkgs.stdenv.hostPlatform.system != "aarch64-linux") {
-      boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-    };
+  config = lib.mkIf (!config.is_microvm) {
+    boot.binfmt.emulatedSystems =
+      lib.lists.filter (sys: pkgs.stdenv.hostPlatform.system != sys) [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+  };
 }
