d95deb2c32cb012e84a343d41dc4174b985fcbd8
haennes <hannes.hofmuth@gmail.com>
2025-01-01 15:12:34 +0100

    dea nextcloud

---
+++ servers/dea/nextcloud.nix
@@ -0,0 +1,90 @@
+{ pkgs, config, lib, ... }:
+let
+  owner = config.services.syncthing.user;
+  group = config.services.syncthing.group;
+  NCdataDir = "/data/nextcloud";
+  PGdataDir = "/data/pg";
+  SCdataDir = config.services.syncthing.dataDir;
+  IPFSdataDir = config.services.kubo.dataDir;
+in {
+
+  age.secrets = {
+    nextcloud_adminpass = {
+      file = ../../secrets/nextcloud/adminpass.age;
+      inherit group owner;
+    };
+  };
+
+  boot.kernel.sysctl = { "fs.inotify.max_user_watches" = 204800; };
+  services.syncthing = rec {
+    user = "nextcloud";
+    group = user;
+  };
+  services.syncthing_wrapper = { enable = true; };
+  systemd.services."nextcloud-setup".requires = [ "syncthing.service" ];
+  systemd.services."nextcloud-setup".after = [ "syncthing.service" ];
+
+  system.activationScripts.ensure-syncthing-dir-ownership.deps =
+    [ "users" "groups" ];
+  system.activationScripts.ensure-syncthing-dir-permissions.deps =
+    [ "users" "groups" ];
+  system.activationScripts.ensure-dirs-exist = {
+    deps = [ "users" "groups" ];
+    text = ''
+      mkdir -p ${NCdataDir}
+      chown -R ${owner}:${group} ${NCdataDir}
+      mkdir -p ${SCdataDir}
+      chown -R ${owner}:${group} ${SCdataDir}
+      mkdir -p ${IPFSdataDir}
+      chown -R ${owner}:${group} ${IPFSdataDir}
+    '';
+  };
+  #mkdir -p ${PGdataDir}
+  #chown postgres:postgres ${PGdataDir}
+
+  networking.firewall.allowedTCPPorts =
+    [ config.ports.ports.curr_ports.nextcloud.web ];
+
+  #services.postgresql.dataDir = PGdataDir;
+  #services.onlyoffice.enable = true;
+  services.nextcloud = {
+    enable = true;
+    hostName = "cloud.hannses.de";
+    package = pkgs.nextcloud30;
+    https = true;
+
+    home = NCdataDir;
+
+    config = {
+      adminpassFile = config.age.secrets.nextcloud_adminpass.path;
+      #dbtype = "pgsql";
+    };
+    database.createLocally = true; # automatically generate a pgsql db
+    settings = {
+      default_phone_region = "DE";
+      trusted_domains = [ "*" ]; # TODO set trusted domains here
+      enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
+    };
+
+    maxUploadSize = config.nextcloud_max_size;
+    extraApps = with config.services.nextcloud.package.packages.apps; {
+      inherit
+      #contacts
+      #calendar
+      #maps
+        deck groupfolders bookmarks cospend notes polls;
+    };
+  };
+}
