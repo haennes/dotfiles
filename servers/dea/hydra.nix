dfcf0518111c53dca42b8c5707a73921b38c823e
haennes <hannes.hofmuth@gmail.com>
2025-01-05 11:48:42 +0100

    FIXME hydra activation script broken

--- servers/dea/hydra.nix
+++ servers/dea/hydra.nix
@@ -15,18 +15,19 @@ in {
 
   };
 
-  #TODO make this a upstream patch. this fails if this is run when hydra get enabled for the first time
-  system.activationScripts.createHydraUser =
-    lib.mkIf config.services.hydra.enable {
-      text = ''
-        pw=$(cat ${
-          config.age.secrets."hydra/users/hannses.age".path
-        }| tr -d \\n | ${pkgs.libargon2}/bin/argon2 "$(LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c16)" -id -t 3 -k 262144 -p 1 -l 16 -e)
+  #TODO doesnt work
+  #make this a upstream patch. this fails if this is run when hydra get enabled for the first time
+  #system.activationScripts.createHydraUser =
+  #  lib.mkIf config.services.hydra.enable {
+  #    text = ''
+  #      pw=$(cat ${
+  #        config.age.secrets."hydra/users/hannses.age".path
+  #      }| tr -d \\n | ${pkgs.libargon2}/bin/argon2 "$(LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c16)" -id -t 3 -k 262144 -p 1 -l 16 -e)
 
-        runuser -u hydra -- ${pkgs.hydra_unstable}/bin/hydra-create-user hannses --password-hash $pw --role admin
-      '';
-      deps = [ "agenixInstall" ];
-    };
+  #      runuser -u hydra -- ${pkgs.hydra_unstable}/bin/hydra-create-user hannses --password-hash $pw --role admin
+  #    '';
+  #    deps = [ "agenixInstall" ];
+  #  };
 
   nix.buildMachines = [{
     hostName = "localhost";
