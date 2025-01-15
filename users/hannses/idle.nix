2a39268726781013c55216c4a60febfe6865577b
haennes <hannes.hofmuth@gmail.com>
2025-01-15 11:22:54 +0100

    hypridle, thanks yeet

--- users/hannses/idle.nix
+++ users/hannses/idle.nix
@@ -1,19 +1,33 @@
-{ pkgs, scripts, ... }: {
-  services.swayidle = {
+{ pkgs, scripts, ... }:
+let brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+in {
+  services.hypridle = {
     enable = true;
-    timeouts = [
-      {
-        timeout = 90;
-        command = "${scripts.lock} -f --grace 20";
-      }
-      {
-        timeout = 180;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-      }
-    ];
-    events = [{
-      event = "lock";
-      command = "${scripts.lock} -f";
-    }];
+
+    settings = {
+      general = {
+        lock_cmd = "${scripts.lock} -f";
+        unlock_cmd = "pkill -USR1 swaylock";
+
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listener = [
+        {
+          timeout = 120;
+          on-timeout = "${brightnessctl} -s set 10";
+          on-resume = "${brightnessctl} -r";
+        }
+        {
+          timeout = 150;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 300;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
   };
 }
