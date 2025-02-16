460b7afc1c58cb9c9491e14e58117fcebfd91e31
haennes <hannes.hofmuth@gmail.com>
2025-02-16 10:04:34 +0100

    tasksmd fmt

--- modules/tasks.nix
+++ modules/tasks.nix
@@ -1,29 +1,27 @@
-{ lib, config, ... }:
-#let hips = config.ips.ips.ips.default.${config.networking.hostName};
-#in
-{
-  services.tasks_md = {
-    enable = true;
-    enableNginx = lib.mkDefault true;
-    conf = let
-      tports = config.ports.ports.curr_ports.tasks;
-      simple = name: {
-        title = name;
-        port = tports.${name};
-      };
-      base_dir = config.services.syncthing.settings.folders.tasks.path;
-    in lib.lists.map (item:
-      item // {
-        #domain = (if (config.is_client) then "tasks.localhost" else hips.wg0);
-        domain = "tasks.localhost";
-        config_dir = "${base_dir}/${item.title}/config";
-        tasks_dir = "${base_dir}/${item.title}/tasks";
-        base_path = "/${item.title}";
-      }) [
-        (simple "uni")
-        (simple "haushalt")
-        (simple "projekte")
-        (simple "coding")
-      ];
-  };
+{ lib, config, ... }: {
+  services.tasks_md =
+    lib.mkIf (config.services.syncthing.settings.folders ? "tasks") {
+      enable = true;
+      enableNginx = lib.mkDefault true;
+      conf = let
+        tports = config.ports.ports.curr_ports.tasks;
+        simple = name: {
+          title = name;
+          port = tports.${name};
+        };
+        base_dir = config.services.syncthing.settings.folders.tasks.path;
+      in lib.lists.map (item:
+        item // {
+          #domain = (if (config.is_client) then "tasks.localhost" else hips.wg0);
+          domain = "tasks.localhost";
+          config_dir = "${base_dir}/${item.title}/config";
+          tasks_dir = "${base_dir}/${item.title}/tasks";
+          base_path = "/${item.title}";
+        }) [
+          (simple "uni")
+          (simple "haushalt")
+          (simple "projekte")
+          (simple "coding")
+        ];
+    };
 }
