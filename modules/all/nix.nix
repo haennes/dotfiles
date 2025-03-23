378ef4cb4bd12529065da21550d43abb8583ed57
haennes <hannes.hofmuth@gmail.com>
2025-03-23 16:16:48 +0100

    dont let cache point to itself

--- modules/all/nix.nix
+++ modules/all/nix.nix
@@ -1,18 +1,19 @@
-{ inputs, pkgs, lib, ... }:
+{ inputs, pkgs, lib, config, ... }:
 let
   channelPath = "/nix/var/nix/profiles/per-user/root/channels";
   #"/etc/nixpkgs/channel/nixpkgs";
   nix-serve-publicKey_raw = import ../../secrets/nix-serve/dea/pub.nix;
   nix-serve-publicKey = lib.last (lib.splitString ":" nix-serve-publicKey_raw);
+  optionalIfNotDea = opt:
+    lib.optional (config.networking.hostName == "dea") opt;
 in {
   nix = {
     package = pkgs.nix;
     settings = {
-      substituters = [
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-        "http://nix-serve.local.hannses.de" # http is fine, since it is inside the wg0 network
-      ];
+      substituters =
+        [ "https://hyprland.cachix.org" "https://nix-community.cachix.org" ]
+        ++ (optionalIfNotDea
+          "http://nix-serve.local.hannses.de"); # http is fine, since it is inside the wg0 network
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
