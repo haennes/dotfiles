69177132b89dadaa03a89fa09471d75714a1b89d
haennes <hannes.hofmuth@gmail.com>
2025-04-07 14:27:31 +0200

    db ticket change filetype

--- users/hannses/scripts/src/deutschland_ticket_screenshot.nix
+++ users/hannses/scripts/src/deutschland_ticket_screenshot.nix
@@ -1,5 +1,5 @@
 { pkgs, hm-config, ... }:
 let home = hm-config.home.homeDirectory;
 in ''
-  ${pkgs.feh}/bin/feh -FZ ${home}/Documents/DeutschlandTicket.jpg
+  ${pkgs.feh}/bin/feh -FZ ${home}/Documents/DeutschlandTicket.png
 ''
