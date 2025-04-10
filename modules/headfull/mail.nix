da0123842583a9f13588a7fccb27e8e1ccd97ef1
haennes <hannes.hofmuth@gmail.com>
2025-04-10 23:13:28 +0200

    mail broken

--- modules/headfull/mail.nix
+++ modules/headfull/mail.nix
@@ -1,6 +1,8 @@
 { ... }: {
   services.davmail = {
     enable = true;
-    url = "https://exchange.hs-regensburg.de/ews/exchange.asmx";
+    #url = "https://exchange.hs-regensburg.de/ews/exchange.asmx";
+    #url = "https://dc2.hs-regensburg.de";
+    #url = "https://adldap.hs-regensburg.de";
   };
 }
