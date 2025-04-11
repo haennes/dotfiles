93e4399f430285d788ba7c278be9abb07619f703
haennes <hannes.hofmuth@gmail.com>
2025-04-11 17:59:43 +0200

    minecraft server

--- modules/all/minecraft.nix
+++ modules/all/minecraft.nix
@@ -1,6 +1,8 @@
-{ lib, ... }: {
+{ config, lib, ... }:
+let inherit (config.ports.ports) curr_ports;
+in {
   #if a more complex setup is needed use: https://github.com/Infinidoge/nix-minecraft
-  services.minecraft-server = {
+  services.minecraft-server = lib.mkIf config.services.minecraft-server.enable {
     #enable = true;
     eula =
       true; # set to true if you agree to Mojang's EULA: https://account.mojang.com/documents/minecraft_eula
@@ -8,11 +10,12 @@
 
     # see here for more info: https://minecraft.gamepedia.com/Server.properties#server.properties
     serverProperties = {
-      server-port = 25565;
+      server-port = curr_ports.minecraft;
       gamemode = "survival";
       motd = "hannes ist alt xD";
       max-players = 5;
       enable-rcon = true;
+      online-mode = false;
       # This password can be used to administer your minecraft server.
       # Exact details as to how will be explained later. If you want
       # you can replace this with another password.
