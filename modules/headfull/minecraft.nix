cbe7b1e1a2519c47b624a91343ca917eca372152
haennes <hannes.hofmuth@gmail.com>
2025-04-11 18:00:09 +0200

    prismlauncher

--- modules/headfull/minecraft.nix
+++ modules/headfull/minecraft.nix
@@ -4,6 +4,7 @@
     systemPackages = with pkgs; [
       atlauncher # minecraft launcher with mod support
       portablemc # minecraft launcher cmd
+      prismlauncher
     ];
 
     etc = { "minecraft_deps/jre8".source = pkgs.jre8.outPath; };
