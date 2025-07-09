b9dd41b0ea6c518bd5a053c8aaf5e6886997b146
haennes <hannes.hofmuth@gmail.com>
2025-07-10 01:23:58 +0200

    gitlab runner more packages

--- vms/instantiable/pales/gitlab-runner.nix
+++ vms/instantiable/pales/gitlab-runner.nix
@@ -1,9 +1,11 @@
-{ config, ... }: {
+{ lib, config, pkgs, ... }: {
   age.secrets = {
     gitlab-runner-token-file = {
       file = ../../../secrets/gitlabrunners/pales_1/token.age;
     };
   };
+
+  environment.systemPackages = config.services.gitlab-runner.extraPackages;
 
   services.gitlab-runner = {
     enable = true;
@@ -16,5 +18,28 @@
           config.age.secrets.gitlab-runner-token-file.path;
       };
     };
+
+    extraPackages = with pkgs; [
+      screen
+      catch2_3
+      cmake
+      gnumake
+      python3
+      valgrind
+      doxygen
+      gcc
+    ];
   };
+  systemd.services.gitlab-runner.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    User = "gitlab-runner";
+    Group = "gitlab-runner";
+  };
+  users.users.gitlab-runner = {
+    home = "/var/lib/gitlab-runner";
+    group = "gitlab-runner";
+
+    isNormalUser = true;
+  };
+  users.groups.gitlab-runner = { };
 }
