4e49c16ff755160a2a50857fec0cf7420812551c
haennes <hannes.hofmuth@gmail.com>
2025-07-10 01:25:59 +0200

    open spotify automatically

--- users/hannses/scripts/startup.nix
+++ users/hannses/scripts/startup.nix
@@ -2,6 +2,7 @@
 lib.filter (a: a != "") (lib.splitString "\n" ''
   ${pkgs.wpaperd}/bin/wpaperd
   [workspace 2 silent] ${pkgs.firefox}/bin/firefox
+  [workspace 7 silent] ${pkgs.firefox}/bin/firefox --new-windows spotify.com
   [workspace 8 silent] ${pkgs.keepassxc}/bin/keepassxc
   [workspace 9 silent] ${pkgs.signal-desktop}/bin/signal-desktop
   [workspace 9 silent] ${scripts.iamb}
