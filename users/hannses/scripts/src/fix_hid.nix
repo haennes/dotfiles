8146acb71f195fc72a4f546dfdeffa91a3d6b3be
haennes <hannes.hofmuth@gmail.com>
2025-08-04 21:29:47 +0200

    fix hid

--- users/hannses/scripts/src/fix_hid.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, lib, ... }:
-let
-  modprobe = lib.getExe' pkgs.kmod "modprobe";
-  sudo = lib.getExe pkgs.sudo;
-in ''
-  ${sudo} ${modprobe} -r hid_multitouch
-  ${sudo} ${modprobe} hid_multitouch
-''
