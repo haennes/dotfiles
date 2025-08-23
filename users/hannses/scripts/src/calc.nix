3b0be64c40ad9d068c3336a80ed3b47d8a657c49
haennes <hannes.hofmuth@gmail.com>
2025-08-23 22:59:26 +0200

    new calc

--- users/hannses/scripts/src/calc.nix
+++ users/hannses/scripts/src/calc.nix
@@ -1,3 +1,3 @@
-{ pkgs, lib, inputs, ... }: ''
-  ${inputs.menu-calc.packages.x86_64-linux.menucalc-fend}/bin/=
+{ pkgs, lib, inputs, hm-config, ... }: ''
+  ${inputs.menu-calc.packages.x86_64-linux.menucalc-fend}/bin/= --dmenu='${hm-config.programs.rofi.finalPackage}/bin/rofi -dmenu -lines 3'
 ''
