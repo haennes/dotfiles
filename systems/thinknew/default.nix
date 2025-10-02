3d2a7427d96b6490949ea717caa015b9e918db5a
haennes <hannes.hofmuth@gmail.com>
2025-10-02 08:36:49 +0200

    deactivate wordpress

--- systems/thinknew/default.nix
+++ systems/thinknew/default.nix
@@ -8,31 +8,31 @@
   services.wireguard-wrapper.enable = true;
   microvmHost.extInterface = "enp0s25";
 
-  services.wordpress.sites."localhost" = {
-    languages = [ pkgs.wordpressPackages.languages.de_DE ];
-    settings = {
-      WPLANG = "de_DE";
-      WP_MAIL_FROM = "noreply@localhost";
-    };
-    plugins = {
-      inherit (pkgs.wordpressPackages.plugins)
-        static-mail-sender-configurator
-        #wordpress-seo
-        webp-express jetpack merge-minify-refresh disable-xml-rpc
-        #simple-login-captcha
-      ;
-    };
-    #extraConfig = ''
-    #    // Enable the plugin
-    #    if ( !defined('ABSPATH') )
-    #      define('ABSPATH', dirname(__FILE__) . '/');
-    #    require_once(ABSPATH . 'wp-settings.php');
-    #    require_once ABSPATH . 'wp-admin/includes/plugin.php';
-    #    activate_plugin( 'disable-xml-rpc/disable-xml-rpc.php' );
-    #    activate_plugin( 'simple-login-captcha/simple-login-captcha.php' );
-    #    ini_set( 'error_log', '/var/lib/wordpress/localhost/debug.log' );
-    #'';
-  };
+  # services.wordpress.sites."localhost" = {
+  #   languages = [ pkgs.wordpressPackages.languages.de_DE ];
+  #   settings = {
+  #     WPLANG = "de_DE";
+  #     WP_MAIL_FROM = "noreply@localhost";
+  #   };
+  #   plugins = {
+  #     inherit (pkgs.wordpressPackages.plugins)
+  #       static-mail-sender-configurator
+  #       #wordpress-seo
+  #       webp-express jetpack merge-minify-refresh disable-xml-rpc
+  #       #simple-login-captcha
+  #     ;
+  #   };
+  #   #extraConfig = ''
+  #   #    // Enable the plugin
+  #   #    if ( !defined('ABSPATH') )
+  #   #      define('ABSPATH', dirname(__FILE__) . '/');
+  #   #    require_once(ABSPATH . 'wp-settings.php');
+  #   #    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+  #   #    activate_plugin( 'disable-xml-rpc/disable-xml-rpc.php' );
+  #   #    activate_plugin( 'simple-login-captcha/simple-login-captcha.php' );
+  #   #    ini_set( 'error_log', '/var/lib/wordpress/localhost/debug.log' );
+  #   #'';
+  # };
   has_battery = false;
   nixpkgs.overlays = [
     (self: super: {
