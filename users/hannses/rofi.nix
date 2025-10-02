fc79678c8cc1059ce0fcfacb989eabc188c74c79
haennes <hannes.hofmuth@gmail.com>
2025-10-02 08:37:18 +0200

    lock

--- users/hannses/rofi.nix
+++ users/hannses/rofi.nix
@@ -1,7 +1,7 @@
 { pkgs, config, globals, theme, ... }: {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland;
+    package = pkgs.rofi;
 
     cycle = true;
     location = "top";
