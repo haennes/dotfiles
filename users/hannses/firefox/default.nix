59eaaa28bbddfde90a5d23ae65c480f0d25a876b
haennes <hannes.hofmuth@gmail.com>
2025-11-19 18:15:52 +0100

    firefox oth profile

--- users/hannses/firefox/default.nix
+++ users/hannses/firefox/default.nix
@@ -8,42 +8,34 @@ let
 in {
   programs.firefox = {
     enable = true;
-    profiles.default = {
-      isDefault = true;
-      extensions.packages = with addons; [
-        tridactyl
-        ublock-origin
-        floccus
-        keepassxc-browser
-        darkreader
-        ipfs-companion
-        passbolt
-      ];
-      settings = settings.default;
-      search = {
-        force = true;
-        default = "ecosia";
-        engines = lib.my.recursiveMerge (lib.attrValues
-          (inputs.haumea.lib.load {
-            src = ./engines;
-            inputs = engines_inputs;
-            loader = inputs.haumea.lib.loaders.default;
-          }));
-
-        #(import ./alto.nix engines_inputs)
-        #// (import ./bahn.nix engines_inputs)
-        #// (import ./fdroid.nix engines_inputs)
-        #// (import ./github.nix engines_inputs)
-        #// (import ./icons.nix engines_inputs)
-        #// (import ./mail.nix engines_inputs)
-        #// (import ./models.nix engines_inputs)
-        #// (import ./nix.nix engines_inputs)
-        #// (import ./programming.nix engines_inputs)
-        #// (import ./social.nix engines_inputs)
-        #// (import ./typst.nix engines_inputs)
-        #// (import ./websearch.nix engines_inputs)
-        #// (import ./wikipedia.nix engines_inputs)
-        #;
+    profiles = rec {
+      default = {
+        isDefault = true;
+        extensions.packages = with addons; [
+          tridactyl
+          ublock-origin
+          floccus
+          keepassxc-browser
+          darkreader
+          ipfs-companion
+          passbolt
+          clearurls
+        ];
+        settings = settings.default;
+        search = {
+          force = true;
+          default = "ecosia";
+          engines = lib.my.recursiveMerge (lib.attrValues
+            (inputs.haumea.lib.load {
+              src = ./engines;
+              inputs = engines_inputs;
+              loader = inputs.haumea.lib.loaders.default;
+            }));
+        };
+      };
+      oth = default // {
+        isDefault = false;
+        id = 1;
       };
     };
   };
