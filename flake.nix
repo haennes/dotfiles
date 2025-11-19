c3e93b54a5f8ce1ae62ec5bc8880e657011fd9ec
haennes <hannes.hofmuth@gmail.com>
2025-11-19 18:08:16 +0100

    lock

--- flake.nix
+++ flake.nix
@@ -7,10 +7,11 @@
     # allow-import-from-derivation = false; #FIXME remove this, these are ifds
 
     extra-substituters =
-      [ "https://nix-community.cachix.org/" "https://numtide.cachix.org/" ];
+      [ "https://nix-community.cachix.org/" "https://numtide.cachix.org/" "https://microvm.cachix.org"];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
     ];
   };
   inputs = {
@@ -195,6 +196,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    # shell-aliases = { };
 
   };
 
@@ -312,6 +314,8 @@
                 "minecraft-server"
               ];
           };
+          overlaysBuilder = channels:
+            [ (final: prev: { inherit (channels.nixpkgs-stable) firefox; }) ];
         };
         insecure = {
           input = nixpkgs;
