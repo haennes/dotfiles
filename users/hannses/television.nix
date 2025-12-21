6dab20b109a4df98bad111830d100c4ad7f46348
haennes <hannes.hofmuth@gmail.com>
2025-12-22 00:30:36 +0100

    tv reduce channels

--- users/hannses/television.nix
+++ users/hannses/television.nix
@@ -1,54 +1,48 @@
 { lib, pkgs, ... }:
 let
-  inherit (lib) getExe;
+  inherit (lib) getExe mapAttrs;
   nix-search-tv = getExe pkgs.nix-search-tv;
 in {
   programs.television = {
     enable = true;
     enableZshIntegration = true;
-    channels.custome-one.cable_channel = [
+    channels = mapAttrs (n: v: v // { metadata.name = n; }) {
 
-      {
-        name = "git-log";
-        source_command = ''
+      "git-log" = {
+        source.command = ''
           git log --oneline --date=short --pretty="format:%h %s %an %cd" "$@"'';
-        preview_command =
+        preview.command =
           "git show -p --stat --pretty=fuller --color=always {0}";
-      }
-      {
-        name = "git-branch";
-        source_command =
+      };
+      "git-branch" = {
+        source.command =
           ''git --no-pager branch --all --format="%(refname:short)"'';
-        preview_command =
+        preview.command =
           "git show -p --stat --pretty=fuller --color=always {0}";
-      }
-      {
-        name = "git-diff";
-        source_command = "git diff --name-only";
-        preview_command = "git diff --color=always {0}";
-      }
-      {
-        name = "git-files";
-        source_command = "git ls-files";
-        preview_command = "bat -n --color=always {}";
-      }
-      {
-        name = "git-reflog";
-        source_command = "git reflog";
-        preview_command =
+      };
+      "git-diff" = {
+        source.command = "git diff --name-only";
+        preview.command = "git diff --color=always {0}";
+      };
+      "git-files" = {
+        source.command = "git ls-files";
+        preview.command = "bat -n --color=always {}";
+      };
+      "git-reflog" = {
+        source.command = "git reflog";
+        preview.command =
           "git show -p --stat --pretty=fuller --color=always {0}";
-      }
-      {
-        name = "nixpkgs";
-        source_command = "${nix-search-tv} print";
-        preview_command = "${nix-search-tv} preview {}";
-      }
-    ];
+      };
+      "nixpkgs" = {
+        source.command = "${nix-search-tv} print";
+        preview.command = "${nix-search-tv} preview {}";
+      };
+    };
     settings = {
       shell_integration.channel_triggers = {
         "env" = [ "export" "unset" ];
         "dirs" = [ "cd" "ls" "rmdir" ];
-        "files" = [ "mv" "cp" "vim" ];
+        # "files" = [ "mv" "cp" "vim" ];
         "git-branch" = [ "git checkout" "git log" ];
       };
     };
