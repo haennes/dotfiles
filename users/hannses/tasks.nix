03c6a34e784c950118976e4887b20f6530709e3b
haennes <hannes.hofmuth@gmail.com>
2026-01-10 22:46:37 +0100

    taskwarrior add profiles and contexts

--- users/hannses/tasks.nix
+++ users/hannses/tasks.nix
@@ -1,13 +1,21 @@
-{ osConfig, pkgs, ... }:
+{ osConfig, pkgs, lib, ... }:
 let
   ips = osConfig.ips.ips.ips.default;
   ports = osConfig.ports.ports.ports;
+  inherit (lib) concatStringsSep mapAttrs;
 in {
   #https://www.reddit.com/r/taskwarrior/comments/1bt1ixi/sync_setup_for_taskwarrior_30/
   #https://gothenburgbitfactory.org/taskserver-troubleshooting/
   programs.taskwarrior = {
     enable = true;
-    config = {
+    config = let
+      vls = [ "ks" "os" "db" "dd" ];
+      vls_projects = map (e: "project:${e}") vls;
+      vls_projects_not = map (e: "project.not:${e}") vls;
+      columns = "id,due,urgency,project,tags,description.count";
+      labels = "ID,Due,Urg,Proj,tags,Description";
+      sort = "priority-/,project-,description+ ";
+    in rec {
       sync = {
         server = {
           url = "http://task.local.hannses.de:${
@@ -16,6 +24,24 @@ in {
         };
       };
       taskd.trust = "ignore task.local.hannses.de";
+      report = {
+        uni = {
+          inherit columns labels sort;
+          description = "uni tasks";
+          filter = "status:pending and ${concatStringsSep " or " vls_projects}";
+        };
+        priv = {
+
+          inherit columns labels sort;
+          description = "priv tasks";
+          filter =
+            "status:pending and ${concatStringsSep " and " vls_projects_not}";
+        };
+      };
+      context = { } // mapAttrs (_: v: {
+        read = v.filter;
+        write = "";
+      }) report;
     };
     package = pkgs.taskwarrior3;
     extraConfig = ''
