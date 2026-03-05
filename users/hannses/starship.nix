a46e11bb6f1b93b467f0ada0f695de227fbf06ad
haennes <hannes.hofmuth@gmail.com>
2026-03-06 00:40:46 +0100

    starship new

--- users/hannses/starship.nix
+++ users/hannses/starship.nix
@@ -1,92 +1,121 @@
-{ theme, ... }: {
+{ lib, ... }: {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
 
     settings = {
+      # add_newline = true;
 
-      # disable unwanted behaviour
-      add_newline = true;
+      format = lib.concatStrings (lib.map (x: "$" + x) [
+        "username"
+        "directory"
 
-      format =
-        "$python$directory$fossil_branch$hg_branch$pijul_channel$git_branch$docker_context$guix_shell$meson$spack$container$jobs$cmd_duration$battery $character";
+        "git_branch"
+        "git_commit"
+        "git_state"
+        "git_status"
+
+        "container"
+        "docker_context"
+
+        "direnv"
+        "nix_shell"
+
+        "shlvl"
+
+        "jobs"
+
+        "zig"
+        "typst"
+        "rlang"
+        "java"
+        "python"
+        "cpp"
+        "c"
+        "rust"
+
+        "package"
+
+        "status"
+        "cmd_duration"
+        "character"
+
+      ]);
+
+      nix_shell = {
+        format = "[$symbol]($style) ";
+        symbol = " ";
+      };
 
       python = { format = "[\\($virtualenv\\)]($style) "; };
 
+      direnv.disabled = false;
+
+      status = {
+        disabled = false;
+        failure_style = "#00ff00";
+        symbol = "✘";
+        not_executable_symbol = "⚠";
+        not_found_symbol = "∅";
+        sigint_symbol = "⏸";
+        signal_symbol = "⚑";
+      };
+
+      os.disabled = false;
+
+      shlvl = {
+        disabled = false;
+        symbol = " ";
+      };
+
+      cpp = {
+        format = "via [$name $version]($style) ";
+        style = "#f00000";
+        disabled = false;
+      };
+
       directory = {
-        format = "[$read_only]($read_only_style)[$path]($style)";
-        style = "#${theme.color_third}";
-        truncation_length = 2;
+        format = "[$read_only]($read_only_style)[$path]($style) ";
+        read_only = "⚿ ";
+        read_only_style = "green";
+        style = "red";
+        truncation_length = 4;
         disabled = false;
       };
 
       jobs = {
-        format = " [$symbol$number]($style)";
-        symbol = "*";
+        format = "[$symbol$number]($style) ";
+        symbol = "∗";
         disabled = false;
       };
 
       character = {
-        success_symbol = "[➜](green)";
-        error_symbol = "[➜](red)";
-        disabled = false;
-      };
-
-      fossil_branch = {
-        format = " [$symbol$branch](bold purple)";
-        disabled = false;
-      };
-
-      hg_branch = {
-        format = " [$symbol$branch]($style)";
-        disabled = false;
-      };
-
-      pijul_channel = {
-        format = " [$symbol$channel]($style)";
+        success_symbol = "[➜](red)";
+        error_symbol = "[➜](green)";
         disabled = false;
       };
 
       git_branch = {
-        format = " [$symbol$branch](bold purple)";
+        format = "[$symbol$branch](yellow) ";
         disabled = false;
       };
 
       docker_context = {
-        format = " [$symbol$context]($style)";
-        disabled = false;
-      };
-
-      guix_shell = {
-        format = " [$symbol]($style)";
-        disabled = false;
-      };
-
-      meson = {
-        format = " [$symbol$project]($style)";
-        disabled = false;
-      };
-
-      spack = {
-        format = " [$symbol$project]($style)";
+        format = "[$symbol$context]($style) ";
         disabled = false;
       };
 
       cmd_duration = {
-        format = " [$duration]($style)";
-        disabled = false;
-      };
-
-      battery = {
-        format = " [$symbol$percentage]($style)";
+        format = " [$duration]($style) ";
         disabled = false;
       };
 
       container = {
-        format = " [$symbol [$name]]($style)";
+        format = " [$symbol [$name]]($style) ";
         disabled = false;
       };
-
+      # disable unwanted behaviour
+      add_newline = true;
     };
   };
 }
