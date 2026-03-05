aa21d1f79555a7d3664d0625e54461b5ad954f04
haennes <hannes.hofmuth@gmail.com>
2026-03-06 00:45:17 +0100

    move battery management to power.nix

--- modules/headfull/power.nix
+++ modules/headfull/power.nix
@@ -1,8 +1,30 @@
-{ ... }: {
-  services.upower = {
-    enable = true;
-    criticalPowerAction = "HybridSleep";
-    usePercentageForPolicy = true;
-    percentageCritical = 3;
+{ config, lib, ... }:
+{
+  config = lib.mkIf config.has_battery {
+    systemd.services.low-battery-hybrid-sleep = {
+      description = "Hybrid sleep on critical battery";
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+      };
+      script = ''
+        capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+        status=$(cat /sys/class/power_supply/BAT0/status)
+
+        if [ "$status" = "Discharging" ] && [ "$capacity" -le 3 ]; then
+          systemctl hybrid-sleep
+        fi
+      '';
+    };
+
+    systemd.timers.low-battery-hybrid-sleep = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "2m";
+        OnUnitActiveSec = "1m";
+        Unit = "low-battery-hybrid-sleep.service";
+      };
+    };
+    services.power-profiles-daemon.enable = true;
   };
 }
