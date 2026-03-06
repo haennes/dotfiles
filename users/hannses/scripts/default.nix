ca1c460f6878f01a7f22a3ce1f75f415a0f33ef1
haennes <hannes.hofmuth@gmail.com>
2026-03-06 01:00:39 +0100

    fmt scripts + nu script

--- users/hannses/scripts/default.nix
+++ users/hannses/scripts/default.nix
@@ -1,19 +1,42 @@
-{ inputs, pkgs, lib, config, ... }@all_inputs:
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}@all_inputs:
 let
   theme = config.home-manager.extraSpecialArgs.theme;
   globals = config.home-manager.extraSpecialArgs.globals;
   scripts = config.home-manager.extraSpecialArgs.scripts;
-  scripts_input = all_inputs // { inherit globals theme scripts; };
+  scripts_input = all_inputs // {
+    inherit globals theme scripts;
+  };
   importNixScript = name: value: pkgs.pkgs.writeShellScript name value;
   importShellScript = name: {
-    "${name}" =
-      pkgs.pkgs.writeShellScript "${name}" "${lib.readFile ./src/${name}.sh}";
+    "${name}" = pkgs.pkgs.writeShellScript "${name}" "${lib.readFile ./src/${name}.sh}";
   };
   hlib = inputs.haumea.lib;
-in lib.mapAttrs (name: value: importNixScript name value) (hlib.load {
-  src = ./src;
-  loader = hlib.loaders.default;
-  inputs = scripts_input;
-}) // {
-  startup = import ./startup.nix scripts_input;
-} # isnt actually a shell-script
+in
+lib.mapAttrs (name: value: importNixScript name value) (
+  hlib.load {
+    src = ./src;
+    loader = hlib.loaders.default;
+    inputs = scripts_input;
+  }
+)
+// {
+  startup = import ./startup.nix scripts_input; # isnt actually a shell-script
+  disable_ext_monitors = pkgs.writers.writeNu "disable_ext_monitors" ''
+    def disable_ext_monitors [] {
+      let monitors = hyprctl monitors -j
+      let ext_monitors =  $monitors | from json | where { |e| $e.name != "eDP-1"}
+      for m in $ext_monitors {
+        echo "disabling monitor" $m.name
+        ^hyprctl keyword monitor $m.id,disable
+      }
+    }
+
+    disable_ext_monitors 
+  '';
+}
