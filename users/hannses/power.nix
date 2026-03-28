36ed00ede66f20d000d219dca39b1ecb1ba7df31
borbitter <git@addy.hannses.de>
2026-03-28 16:44:54 +0100

    fix constant batsignal spam

--- users/hannses/power.nix
+++ users/hannses/power.nix
@@ -1,7 +1,15 @@
-{ lib, osConfig, ... }:
+{ pkgs, osConfig, ... }:
 {
   services.batsignal = {
     enable = osConfig.has_battery;
+    package = pkgs.batsignal.overrideAttrs {
+      src = pkgs.fetchFromGitHub {
+        owner = "Bootjewolf";
+        repo = "batsignal";
+        rev = "23f9d9b8c061b55501e21d6e89fa4c523254f73a";
+        hash = "sha256-kGj5FVfZJPX/TCsCT2X+gplz1YAg0GlmGDMjCCxtZZw=";
+      };
+    };
     extraArgs = [
       # warning levels
       "-w"
@@ -19,6 +27,7 @@
       "80"
 
       "-p" # show message when changing charging state
+      "-q" # show message only once
       "-P"
       "charging"
 
