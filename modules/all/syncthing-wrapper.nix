19c3bb29b762e263bad4d435ea04b2a6f576b5a2
borbitter <git@addy.hannses.de>
2026-06-12 14:16:24 +0200

    fix(syncthing)!: disable versioning for Signal Backups

--- modules/all/syncthing-wrapper.nix
+++ modules/all/syncthing-wrapper.nix
@@ -188,6 +188,7 @@ in
         // servers;
         "hannses__SignalBackup".devices = {
           inherit handyHannes;
+          versioning = null; #disable versioning to not overwhelm server storage
         }
         // servers;
         "hannses__DownloadHandy".devices = {
