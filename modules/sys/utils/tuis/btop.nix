47e882cd4173a5e44ccfe382ec5bc2f2296912d3
borbitter <git@addy.hannses.de>
2026-07-22 00:42:30 +0200

    feat(sys/utils/tuis/btop): also enable on metal servers

--- modules/sys/utils/tuis/btop.nix
+++ modules/sys/utils/tuis/btop.nix
@@ -9,7 +9,7 @@ let
 in
 {
   options.my.utils.tuis.btop.enable = mkEnableOption "btop" // {
-    default = config.my.utils.tuis.enable;
+    default = config.my.utils.tuis.enable || (config.is_server && !config.is_microvm);
   };
   config = mkIf config.my.utils.tuis.btop.enable {
     environment.systemPackages = with pkgs; [
