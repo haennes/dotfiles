53fb04c4f894df3d75d05b30b90fcf7a296109b8
borbitter <git@addy.hannses.de>
2026-07-29 11:21:10 +0200

    fix(modules/hm/monitors): fix only eDP config

--- modules/hm/desktop/monitor-profiles/$Samsung Display Corp.0x4187eDP-1.nix
+++ modules/hm/desktop/monitor-profiles/$Samsung Display Corp.0x4187eDP-1.nix
@@ -3,9 +3,9 @@
   {
     "adaptiveSync": false,
     "mode": "1920x1200",
-    "position": "3000,0",
+    "position": "0,0",
     "scale": 1.0,
     "transform": "normal",
-    "criteria": "Samsung Display Corp. 0x4187 "
+    "criteria": "Samsung Display Corp. 0x4187 Unknown"
   }
 ]''
