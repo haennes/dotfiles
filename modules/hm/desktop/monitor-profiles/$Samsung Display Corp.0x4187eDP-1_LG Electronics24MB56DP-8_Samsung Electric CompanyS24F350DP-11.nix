01dad6d13ff24b1163b5557addc8cbe021613f8e
borbitter <git@addy.hannses.de>
2026-09-05 11:38:57 +0200

    feat(modules/hm/desktop/monitor-profiles): add new monitor profiles

--- modules/hm/desktop/monitor-profiles/$Samsung Display Corp.0x4187eDP-1_LG Electronics24MB56DP-8_Samsung Electric CompanyS24F350DP-11.nix
+++ modules/hm/desktop/monitor-profiles/$Samsung Display Corp.0x4187eDP-1_LG Electronics24MB56DP-8_Samsung Electric CompanyS24F350DP-11.nix
@@ -3,7 +3,7 @@
   {
     "adaptiveSync": false,
     "mode": "1920x1200",
-    "position": "0,0",
+    "position": "3698,1106",
     "scale": 1.0,
     "transform": "normal",
     "criteria": "Samsung Display Corp. 0x4187 Unknown"
@@ -11,16 +11,16 @@
   {
     "adaptiveSync": false,
     "mode": "1920x1080",
-    "position": "1920,0",
-    "scale": 1.0,
+    "position": "0,0",
+    "scale": 0.832031,
     "transform": "90",
     "criteria": "LG Electronics 24MB56 0x01010101"
   },
   {
     "adaptiveSync": false,
     "mode": "1920x1080",
-    "position": "3000,0",
-    "scale": 1.0,
+    "position": "1298,656",
+    "scale": 0.800781,
     "transform": "normal",
     "criteria": "Samsung Electric Company S24F350 H4ZMA05329"
   }
