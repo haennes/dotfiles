8b722816aed155c778d9e07afae9ff434213c0b7
borbitter <git@addy.hannses.de>
2026-09-21 21:53:15 +0200

    feat(modules/hm/ai/mux): keybindings init

--- modules/hm/ai/mux.nix
+++ modules/hm/ai/mux.nix
@@ -14,6 +14,19 @@ in
   config = mkIf config.my.ai.mux.enable {
     programs.herdr = {
       enable = true;
+      settings = {
+        keys = {
+          navigate_workspace_up = "k";
+          navigate_workspace_down = "j";
+          next_workspace = "n";
+          previous_workspace = "p";
+          next_agent = "h";
+          previous_agent = "l";
+        };
+        onboarding = false;
+        ui.prompt_new_workspace_name = true;
+        experimental.kitty_graphics = true;
+      };
     };
   };
 }
